c3caca18dce9ce7ff353fa2284234818027973c1
Ken Albers <ken.albers@gmail.com>
2017-06-30 15:09:35 -0400

    Tweaks

--- items/show.php
+++ items/show.php
@@ -5,15 +5,16 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
 
 <h1><?php echo metadata('item', 'display_title'); ?></h1>
 
-<?php echo all_element_texts('item'); ?>
-
-<!-- The following returns all of the files associated with an item. -->
 <?php if (metadata('item', 'has files')): ?>
 <div id="itemfiles" class="element">
-    <h3><?php echo __('Files'); ?></h3>
     <div class="element-text"><?php echo files_for_item(array('imageSize'=>'fullsize')); ?></div>
 </div>
 <?php endif; ?>
+
+<?php echo all_element_texts('item'); ?>
+
+<!-- The following returns all of the files associated with an item. -->
+
 
 <!-- If the item belongs to a collection, the following creates a link to that collection. -->
 <?php if (metadata('item', 'Collection Name')): ?>
@@ -44,11 +45,5 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
 
 <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
-<nav>
-<ul class="item-pagination navigation">
-    <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-    <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-</ul>
-</nav>
 
 <?php echo foot(); ?>
