5df2e49ba9f42ec95559e50bcecedf07fa293c49
Ken Albers <ken.albers@gmail.com>
2017-07-10 13:40:39 -0400

    Remove featured requirement from home page exhibit list

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
 
 
     <div class="large-12 columns large-up-3 medium-up-2">       
-        <?php $exhibits = get_records('Exhibit', array('featured'=>true, 'sort_field'=>'random')); ?>
+        <?php $exhibits = get_records('Exhibit', array('sort_field'=>'random')); ?>
         <?php foreach($exhibits as $exhibit): ?>
             <div class="large-4 column column-block exhibit">
                     <?php if ($exhibitImage = record_image($exhibit, 'square_thumbnail', array('class' => 'home-exhibit circle'))): ?>
