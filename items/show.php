7f3d112e3604b3604b78ae7d6616338b44697cb9
Ken Albers <ken.albers@gmail.com>
2017-08-16 11:24:58 -0400

    Change citation text.

--- items/show.php
+++ items/show.php
@@ -34,7 +34,7 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
 
 <!-- The following prints a citation for this item. -->
 <div id="item-citation" class="element">
-    <h3><?php echo __('Citation'); ?></h3>
+    <h3><?php echo __('How to cite this item:'); ?></h3>
     <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
 </div>
 
