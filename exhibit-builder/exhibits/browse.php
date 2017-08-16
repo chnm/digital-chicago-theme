a89bf3348f8a1ff3da0225ebc135346505ac3f95
Ken Albers <ken.albers@gmail.com>
2017-08-16 14:22:36 -0400

    Fixed to show all exhibit tags

--- exhibit-builder/exhibits/browse.php
+++ exhibit-builder/exhibits/browse.php
@@ -5,7 +5,7 @@ echo head(array('title' => $title, 'bodyclass' => 'exhibits browse'));
 <p class="orientation large-10 large-offset-1 columns">Explore Digital Chicago!<span class="show-for-medium"> Use the tags below to filter our faculty projects.</span></p>
 
 <div class="tag-cloud align-spaced show-for-medium column">
-<?php $tags = get_records('Tag', array('type'=>'exhibit' )); ?>
+<?php $tags = get_records('Tag', array('type'=>'exhibit' ),0); ?>
 <?php echo tag_cloud($tags, 'exhibits/browse'); ?>
 </div>
 <div class="exhibits">
